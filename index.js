3accff75cc2e0b2d1a5bcdf0440d0418f3f1af61
Erich <eheiseler@gmail.com>
2022-02-23 18:49:51 -0500

    commit tree 883ab8b5f17432dfbca65abe35f37019b0840f0f

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name]
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats]
+    return copyOfCats
+}
+
+function removeLastCat() {
+    const newArr = cats.slice(0, 2)
+    return newArr
+}
+
+function removeFirstCat() {
+    const newArr = cats.slice(1)
+    return newArr
+}
